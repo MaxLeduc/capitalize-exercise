e2c8b7afe1fcc27a9d159c70528d9b5ca2b3f17f
Mona Lisa <leduc.maximep@gmail.com>
2019-11-21 18:10:31 -0500

    polish

--- index.test.js
+++ index.test.js
@@ -4,6 +4,7 @@ const capitalize = require('./index');
 // if capitalize is a function
 // if it capitalizes the first letter of every word of the string passed to it
 
+// NOTE: run me by using `jest` or `jest index.test.js --watch` in this directory.
 describe('capitalize', () => {
   it('', () => {
     // your custom test here.
