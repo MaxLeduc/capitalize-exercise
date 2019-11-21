2e40e6c9a458d7239c5d43b836a17ad5e848e57a
Mona Lisa <leduc.maximep@gmail.com>
2019-11-21 17:45:56 -0500

    polish

--- index.test.js
+++ index.test.js
@@ -1,3 +1,11 @@
 const capitalize = require('./index');
 
-test('it is a function', () => {})
+// --- Let's test for:
+// if capitalize is a function
+// if it capitalizes the first letter of every word of the string passed to it
+
+describe('capitalize', () => {
+  it('', () => {
+    // your custom test here.
+  })
+})
